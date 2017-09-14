f272342a7feb31cf0239ca24ed6fb0132fb77fa6
Mike <huanghaiying@bigbaser.com>
2017-09-14 10:48:01 +0800

    update namespace

--- src/Module.php
+++ src/Module.php
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * @inheritdoc
      */
-    public $controllerNamespace = 'cy\swagger\controllers';
+    public $controllerNamespace = 'mike\swagger\controllers';
 
     /**
      * @inheritdoc
